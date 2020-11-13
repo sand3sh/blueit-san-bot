b807ff90d0d807f18a21bf32057fcbf8edb20404
sand3sh <sand3sh@gmail.com>
2020-11-13 07:51:26 +0530

    fixing pushshift issue

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,8 @@ SCORE_THRESHOLD = 0  # downvote
 SUBREDDIT_THRESHOLD = 5000
 TOP_SUBREDDIT_NUM = 10  # number of subreddits to search for repost-able content
 MIN_SCORE = 0  # for posts to repost
-SUBMISSION_SEARCH_TEMPLATE = f"https://api.pushshift.io/reddit/search/submission/?after={{after}}&before={{before}}&sort_type=score&sort=desc&subreddit={{subreddit}}&score=>{SUBREDDIT_THRESHOLD}"
+#update
+SUBMISSION_SEARCH_TEMPLATE = f"https://api.pushshift.io/reddit/search/submission/?after={{after}}&before={{before}}&sort_type=score&sort=desc&subreddit={{subreddit}}&score>={SUBREDDIT_THRESHOLD}"
 DAY = 86400  # POSIX day (exact value)
 MINUTE = 60
 #custom command s@n
